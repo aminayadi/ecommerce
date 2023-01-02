6474fe0f7d3277faf35415f3a8dbd94adc7dd091
= <=>
2023-01-02 10:13:49 +0100

    Initial version of chatmanagerdb generated by generator-jhipster@7.9.3

---
+++ src/test/java/com/ecommerce/chatmanager/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.ecommerce.chatmanager;
+
+import com.ecommerce.chatmanager.ChatmanagerdbApp;
+import com.ecommerce.chatmanager.config.AsyncSyncConfiguration;
+import com.ecommerce.chatmanager.config.EmbeddedMongo;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { ChatmanagerdbApp.class, AsyncSyncConfiguration.class })
+@EmbeddedMongo
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
