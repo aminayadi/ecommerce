d5f3be1158a1b46aba3e8e4fc9d32eb700302bfa
= <=>
2023-01-11 09:28:00 +0100

    Initial version of photodb generated by generator-jhipster@7.9.3

---
+++ src/test/java/com/ecommerce/photomanager/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.ecommerce.photomanager;
+
+import com.ecommerce.photomanager.PhotodbApp;
+import com.ecommerce.photomanager.config.AsyncSyncConfiguration;
+import com.ecommerce.photomanager.config.EmbeddedMongo;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { PhotodbApp.class, AsyncSyncConfiguration.class })
+@EmbeddedMongo
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
